16444eddf781919cc4e93ad5ea9cf12093da1fb3
Mehul <mehulg8142@gmail.com>
2025-01-25 13:51:11 -0500

    loda

--- petproject/src/App.js
+++ petproject/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import react from 'react';
 import './App.css';
+import Home from './components/Home'
 
 function App() {
   return (
     <div className="App">
-      
+      <Home />
     </div>
   );
 }
--- petproject/src/components/Home.js
+++ petproject/src/components/Home.js
@@ -1,3 +1,24 @@
 import react from "react";
+import LOGO from '../assets/images/logo.png'
+import BG from '../assets/images/3481301037-preview.mp4'
+
+const Home = () => {
+
+    return (
+        <div>
+
+            <video style={{position:"fixed", zIndex:"-1", width:"100%"}} className='videoTag' autoPlay loop muted>
+                <source src={BG} type='video/mp4' />
+            </video>
+            <button>Login</button>
+            <button>Sign Up</button>
+
+        </div>
+    );
 
 
+
+}
+
+
+export default Home;
